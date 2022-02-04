6048eb8f856c0547092af6ee21063b937567acb8
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-02-04 22:58:20 +0530

    selenium

---
+++ src/test/java/selenium07/XpathExample.java
@@ -0,0 +1,13 @@
+package selenium07;
+
+import selenium04.BaseClass;
+
+public class XpathExample extends BaseClass{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		setup();
+		getUrl("");
+	}
+
+}
