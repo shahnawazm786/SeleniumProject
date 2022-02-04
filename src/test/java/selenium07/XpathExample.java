87d3767cb44cd0d1d11efac7d6fbc49e87b8478a
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-02-04 23:10:32 +0530

    selenium

--- src/test/java/selenium07/XpathExample.java
+++ src/test/java/selenium07/XpathExample.java
@@ -1,5 +1,7 @@
 package selenium07;
 
+import org.openqa.selenium.By;
+
 import selenium04.BaseClass;
 
 public class XpathExample extends BaseClass{
@@ -7,7 +9,17 @@ public class XpathExample extends BaseClass{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		setup();
-		getUrl("");
+		getUrl("file:///C:/Users/ADMIN/Desktop/HTMLPage/Entry.html");
+		getAttributeExample();
+		getTextExample();
+	}
+	static void getAttributeExample() {
+		String place=driver.findElement(By.xpath("(//input[@id='name'])[1]")).getAttribute("placeholder");
+		System.out.println(place);
+	}
+	static void getTextExample() {
+		String text=driver.findElement(By.xpath("//label[@for='male']")).getText();
+		System.out.println(text);
 	}
 
 }
