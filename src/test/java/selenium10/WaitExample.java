011dd46374f7200bd638fce400c65b43ff28d24b
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-02-16 14:21:58 +0530

    screenshot

--- src/test/java/selenium10/WaitExample.java
+++ src/test/java/selenium10/WaitExample.java
@@ -1,29 +1,42 @@
 package selenium10;
 
+import java.io.File;
+import java.io.IOException;
+import java.sql.Date;
 import java.time.Duration;
+import java.time.LocalDateTime;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import selenium04.BaseClass;
 
 public class WaitExample extends BaseClass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		setup();
 		getUrl("file:///C:/Users/ADMIN/Desktop/HTMLPage/JSExample.html");
 		getButton();
-		getButtonExplicitWait();
+		//getButtonExplicitWait();
 	}
 	
-	static void getButton() {
+	static void getButton() throws IOException {
 		// implicit wait
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		try {
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
 		WebElement ele=driver.findElement(By.id("Button2"));
 		ele.click();
+		}catch(Exception ex) {
+			File scrFile=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			  LocalDateTime now = LocalDateTime.now();  
+			String fileName=now.toString().replace('-', '_').replace(':', '_').replace(".", "_");
+			System.out.println(fileName);
+			FileUtils.copyFile(scrFile, new File("src/test/resources/screenshot/"+fileName+".png"));
+			driver.quit();
+			 
+		}
 	}
 	static void getButtonExplicitWait() {
 		WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(10));
