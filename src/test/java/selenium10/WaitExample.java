232a0442de4d9e75d478a0cd41a6666d38c04690
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-02-16 14:28:24 +0530

    element

--- src/test/java/selenium10/WaitExample.java
+++ src/test/java/selenium10/WaitExample.java
@@ -18,8 +18,9 @@ public class WaitExample extends BaseClass {
 		// TODO Auto-generated method stub
 		setup();
 		getUrl("file:///C:/Users/ADMIN/Desktop/HTMLPage/JSExample.html");
-		getButton();
+		//getButton();
 		//getButtonExplicitWait();
+		takeScreenshotElement();
 	}
 	
 	static void getButton() throws IOException {
@@ -39,11 +40,19 @@ public class WaitExample extends BaseClass {
 		}
 	}
 	static void getButtonExplicitWait() {
+		
 		WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(10));
 		//wait.pollingEvery(Duration.ofMillis(100));
 		WebElement ele=wait.pollingEvery(Duration.ofMillis(100))
 		.until(ExpectedConditions.elementToBeClickable(By.id("Button2")));
 		ele.click();
+	}
+	static void takeScreenshotElement() throws IOException {
+		WebElement ele=driver.findElement(By.id("Button2"));
+		ele.click();
+		File srcFile=ele.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(srcFile, new File("src/test/resources/screenshot/element/ele.png"));
+		driver.quit();
 	}
 
 }
